ca8d516d01a0da14dd0cbee14c69f8aa4a61a34f
MaJIoU07 <58075552+MaJIoU07@users.noreply.github.com>
2020-03-04 16:34:35 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -56,3 +56,62 @@ echo "<!DOCTYPE html><html><head><meta charset='utf-8'><meta name='viewport' con
 		myfunc("D:\WebServers\home\testsite\www\myfile.txt", "m");
 	?>
 </div>
+<?php
+
+function getAbbreviation($name)
+{
+	$name = mb_strtolower($name);
+	$result = mb_strtoupper( mb_substr($name, 0, 1) );
+	$position = '';
+	for($i = 1; $i < mb_strlen($name); $i++){
+		$symbol = mb_substr($name, $i, 1) ;
+		if($position == 'next'){
+			$symbol = mb_strtoupper($symbol);
+			$result .= $symbol;
+		}
+		if($symbol == ' '){
+			$position = 'next';
+		}else{
+			$position = '';
+		}
+
+	}
+	return $result;
+}
+var_dump( getAbbreviation("донбасская ГОСУДАРСТВЕННАЯ машиностроительная академия") );
+function truncate_string($str, $maxsymbol)
+{
+	$symb = '...';
+	for ($i=0; $i < mb_strlen($str); $i++) { 
+		$symbol = mb_substr($str, $i, 1) ;
+		if ($i < $maxsymbol) {
+			$res .= $symbol;
+		}else{
+			$res .= $symb;
+			break;
+		}
+	}
+	return $res;
+}
+var_dump( truncate_string("донбасская", 7) );
+function getCountSymbol($stroka, $symb)
+{
+	$count = 0;
+	for ($i=0; $i < mb_strlen($stroka); $i++) {
+		$symbol = mb_substr($stroka, $i, 1);
+		if ($symbol == $symb) {
+			$count++;
+		}
+	}
+	echo "Количество искомых символов $symb равно ", $count;
+}
+getCountSymbol("телефон", "е");
+function getShortFio($str)
+{
+	$FIO = explode(" ", $str);
+	$FIO[1] = mb_strtoupper(mb_substr($FIO[1], 0, 1));
+	$FIO[2] = mb_strtoupper(mb_substr($FIO[2], 0, 1));
+	$shortFIO .= $FIO[0]." ".$FIO[1].".".$FIO[2].".";
+	echo $shortFIO;
+}
+getShortFio("Иванов Иван Иванович");
